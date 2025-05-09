7b4a253c0e09b3d2c64bf45a11f819faab000da4
ckendrick-mqu <christopher.kendrick1@students.mq.edu.au>
2025-05-09 12:32:19 +1000

    Colliders

---
+++ Assets/Tiled/TilemapGame/TinyDungeonTilesheet.tsx
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeonTilesheet" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <image source="tilemap.png" width="203" height="186"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="4">
+   <object id="4" x="10" y="9.25" width="6.125" height="6.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.125" y="10.0625" width="16.25" height="5.8125"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8.875" width="5.75" height="7.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.125" y="0" width="5.875" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="5.875" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.375" y="-0.125" width="5.875" height="8.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="8.8125"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="5">
+   <object id="4" x="0" y="0" width="4.375" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="8.625" y="7.375" width="0.125"/>
+   <object id="3" x="7.875" y="8" height="0.625"/>
+   <object id="5" x="24.875" y="-15" height="0.125"/>
+   <object id="6" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
